21cf073581117d839272acb4d0fb516c3a72af6d
Romerito Campos <romerito.campos@gmail.com>
2014-02-27 20:45:21 -0300

    partitions redefined

--- mpp_algorithms/Chen2000/lowerbound.cpp
+++ mpp_algorithms/Chen2000/lowerbound.cpp
@@ -1,32 +1,15 @@
 #include <stdio.h>
 #include "lowerbound.h"
-#include "network.h"
-
-using namespace rca;
-
-int * partitions;
-
-void alloc_partition (int part_size) {
-	
-	partitions = (int*) calloc (part_size, sizeof(int));
-	
-}
-
-void dealloc_partition () {
-	
-}
-
-void configure_patition (const vector<Group*> & group) {
-	
-}
 
 int main (void){
 
-	alloc_partition (10);
+	Partition p;
+	Partition q;
 	
-	for (int i = 0; i < 10; i++) {
-		printf ("%d \n", partitions[i]);
-	}
+	q.add_node (1);
+	
+	p.join(q);
+	
 	
 	return 0;
 }
--- mpp_algorithms/Chen2000/lowerbound.h
+++ mpp_algorithms/Chen2000/lowerbound.h
@@ -1,14 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <set>
+#include <memory>
 #include "group.h"
+
+#include <memory>
 
 using namespace std;
 using namespace rca;
 
-extern int * patitions;
-
-void alloc_partition (int part_size);
-void dealloc_partition ();
-
-void configure_patition (const vector<Group*> & group);
+/*
+ * Partition.
+ * 
+ */
+typedef struct _partition {
+	
+	std::set<int> _nodes;
+	std::set<int> _groups;
+	
+	void join ( struct _partition p) {
+	
+		std::set<int> _set = p.get_nodes();
+		for (int j : _set) {
+			add_node (j);
+		}
+		
+		_set = p.get_groups();
+		for (int j : _set) {
+			add_group (j);
+		}
+		
+	};
+	
+	void add_node (int n) {
+		_nodes.insert (n);
+	};
+	
+	void add_group (int n) {
+		_nodes.insert (n);
+	};
+	
+	std::set<int> get_nodes () {
+		return _nodes;
+	};
+	
+	std::set<int> get_groups () {
+		return _nodes;
+	};
+	
+	
+}Partition;
