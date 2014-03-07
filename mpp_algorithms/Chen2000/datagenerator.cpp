f9ba9abfacff00f69a3575911b6aaffe8e01674c
Romerito Campos <romerito.campos@gmail.com>
2014-03-07 14:29:33 -0300

    constructor added

--- mpp_algorithms/Chen2000/datagenerator.cpp
+++ mpp_algorithms/Chen2000/datagenerator.cpp
@@ -3,7 +3,37 @@
 using namespace std;
 using namespace rca;
 
+Generator::Generator (std::string file) {
+
+	Reader r (file);
+	
+	init ();
+	
+	r.configNetwork ( net.get() );
+	
+}
+
+void Generator::init () {
+	
+	Network _net;
+	net = make_shared<Network> (_net);
+	
+}
+
+void Generator::gen_network () {
+	
+	std::set<Link> links = net.get ()->getLinks ();
+	
+	for (Link l : links) {
+		cout << l << endl;
+	}
+	
+}
+
+
 int main (void) {
+	
+	Generator g("/worksace/Doutorado/b30_1.brite");
 	
 	return 0;
 }
--- mpp_algorithms/Chen2000/datagenerator.h
+++ mpp_algorithms/Chen2000/datagenerator.h
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <fstream>
 
 #include "group.h"
 #include "network.h"
+#include "reader.h"
 
 namespace rca {
 
@@ -19,6 +21,8 @@ public:
 private:
 	std::shared_ptr< rca::Network > net;
 	std::vector< std::shared_ptr<Group> > groups;
+	
+	void init ();
 
 };
 
