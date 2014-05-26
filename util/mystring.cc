970e9075e87838bd9ad5d652bfb7d2014c55b0a2
Romerito Campos <romerito.campos@gmail.com>
2014-05-26 12:39:06 -0300

    teste bin for mystring added

--- util/mystring.cc
+++ util/mystring.cc
@@ -2,3 +2,14 @@
 
 using namespace rca;
 
+std::string get_instance_name (std::string file) {
+	
+	size_t end = file.find ('.');
+	
+	for (size_t i=0; i < end; i++) {
+		std::cout << file.at (end);
+	}
+	cout << endl;
+	
+	return "";
+}
