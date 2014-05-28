48fc9f43dbe9214dbc55058d1ad990f9d285ff76
Romerito Campos <romerito.campos@gmail.com>
2014-05-28 11:09:34 -0300

    new method's declaration added to chenalgorithm.h

--- mpp_algorithms/Chen2000/chenalgorithm.h
+++ mpp_algorithms/Chen2000/chenalgorithm.h
@@ -5,14 +5,30 @@
 #include <vector>
 #include <string>
 
+typedef struct sttree {
+	
+	
+}STTree;
+
 class Chen {
 
 public: 
 	Chen (std::string input);
 	
+	/*pre processing step*/
+	void pre_processing ();
+	
 private:
+	//matrix that stores the congestion over each egde
 	std::vector< std::vector<int> > edges;
+	
+	//current max congestion
 	double congestion;
+	
+	//the parameter that indicates the dilation
+	int alpha;
+	
+	std::vector<STTree> tree;
 	
 };
 
