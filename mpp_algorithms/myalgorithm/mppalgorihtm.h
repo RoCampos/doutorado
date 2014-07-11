49a3d710c35df046e642976b34c2211ffb829403
Romerito Campos <romerito.campos@gmail.com>
2014-07-11 15:24:57 -0300

    Algorithm class defined

---
+++ mpp_algorithms/myalgorithm/mppalgorihtm.h
@@ -0,0 +1,19 @@
+#ifndef _MYCHENALGORITHM_
+#define _MYCHENALGORITHM_
+
+#include <iostream>
+#include <string>
+
+class MPPAlgorithm {
+  
+public:
+  MPPAlgorithm ();
+  
+private:
+  std::string m_algol_info;
+  std::string m_problem;
+  int m_objectives;
+    
+};
+
+#endif
