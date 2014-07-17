8a92321dd16dad2bd267db5e086b94f4e045b56b
Romerito Campos <romerito.campos@gmail.com>
2014-07-17 14:56:24 -0300

    Some comments added to preprocessing.sh

--- scripts/preprocesssing.sh
+++ scripts/preprocesssing.sh
@@ -3,9 +3,17 @@
 #preprocessamento necessário para entrada no algoritmo de Chen.
 #Vai gera um arquivo com o nome da instancia e extensão .pre
 
+
+#obtém os nomes das instâncias
 instances=`ls -v $1`
+
+#diretório onde se encontram as instâncias solucionadas: FORMATO: b30_19.brite_st_9.dat.log
 input_dir=$1
+
+#diretório onde se colocada a saída: b30_19.pre
 output_dir=$2
+
+#bin = filerSol
 bin=$3
 
 #sed expression to get the name of the file
