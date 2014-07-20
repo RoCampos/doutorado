460bcd91564c28d24d652799b3999f344a14433f
Romerito Campos <romerito.campos@gmail.com>
2014-07-20 12:04:47 -0300

    steiner tree prunnig is not working..

--- mpp_algorithms/myalgorithm/steiner_test.cpp
+++ mpp_algorithms/myalgorithm/steiner_test.cpp
@@ -4,6 +4,7 @@
 #include "reader.h"
 #include "path.h"
 #include "algorithm.h"
+#include "steinertree.h"
 
 using namespace std;
 using namespace rca;
@@ -12,6 +13,8 @@ void print_terminals (Group & group);
 std::vector<Path> getPaths (Group & group, Network & net);
 void print_paths (std::vector<Path> & paths);
 
+SteinerTree create_steiner_tree (std::vector<Path> & paths, 
+				 Network & net, Group & group);
 
 int main (int argv, char**argc) {
 
@@ -29,6 +32,16 @@ int main (int argv, char**argc) {
     //printing the paths: terminals to terminals
     std::vector<Path> paths = getPaths (*groups[0], network);
     print_paths (paths);
+    
+    SteinerTree st = create_steiner_tree (paths, network, *groups[0]);    
+    Edge *begin = st.listEdge.head;
+	double cost = 0.0;
+    while (begin != NULL) {
+	cost += begin->cost;
+	begin = begin->next;
+    }
+    cout << "Cost After Prunning: " << cost << endl;
+    st.xdotFormat ();    
     
     return 0;
 }
@@ -70,4 +83,45 @@ void print_paths (std::vector<Path> & paths) {
       cout << *it << endl;
   }
   
+}
+
+SteinerTree create_steiner_tree (std::vector<Path> & paths, 
+				 Network & net, Group & group) {
+  
+  cout << "Building Steiner Tree for Group: " << group.getId () << endl; 
+  
+  std::vector<double> objs(1);
+  
+  unsigned int NODES = net.getNumberNodes();
+  
+  std::vector<SteinerNode> _nodes(NODES, 
+				  SteinerNode(0,0,false));
+  
+  for (unsigned int i=0; i < NODES; i++) {
+    _nodes[0].setIndex (i);
+  }
+  
+  SteinerTree st(1, NODES );
+  st.setTempStructures (objs,_nodes);
+  
+  for (int i=0; i < group.getSize(); i++) {
+    st.setTerminal (group.getMember (i));
+  }
+  st.setTerminal (group.getSource());
+  
+  for (int i=0; i < paths.size (); i++) {
+    Path path = paths[i];
+    
+    for (int i=0; i < path.size ()-1; i++) {
+      int v = path[i];
+      int w = path[i+1];
+      Link link (v,w, net.getCost(v,w));
+      st.addEdge (link.getX(), link.getY(), link.getValue() );
+    }    
+  }
+  
+  cout << "Cost Before Prunning: " << st.getCost () << endl;
+  st.prunning ();
+  
+  return st;
 }
