ce594adb4b639ccad400dc1d87451f9490ae3681
Romerito Campos <romerito.campos@gmail.com>
2014-08-20 20:12:27 -0300

    bstcost implementation file created

---
+++ mpp_algorithms/myalgorithm/bstcost.cpp
@@ -0,0 +1,11 @@
+#include "bstcost.h"
+
+void BreadthSearchCost::make_tree (rca::Group&, rca::Network &, 
+				  std::shared_ptr<SteinerTree> & st) {
+	
+#ifdef DEBUG
+	std::cout << "BreadthSearchCost\n";
+#endif
+	
+	
+}
