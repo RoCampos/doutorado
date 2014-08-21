bbce2527fce8ad69af1684a8c20e313cd79e7c49
Romerito Campos <romerito.campos@gmail.com>
2014-08-21 16:18:01 -0300

    typedef and method to update the queue of edges added

--- mpp_algorithms/myalgorithm/bstcost.h
+++ mpp_algorithms/myalgorithm/bstcost.h
@@ -3,9 +3,17 @@
 
 #include <iostream>
 #include <memory>
+#include <queue>
+#include <functional>
+#include "network.h"
+#include "link.h"
+#include "group.h"
 
 #include "steinertree.h"
 #include "strategy_make_tree.h"
+
+typedef typename std::priority_queue<rca::Link, std::vector<rca::Link>, std::greater<rca::Link> > priority_queue_edge;
+typedef typename std::vector<int>::const_iterator c_iterator;
 
 class BreadthSearchCost : public TreeAlgorithmInterface
 {
@@ -13,6 +21,9 @@ class BreadthSearchCost : public TreeAlgorithmInterface
 public:
 	void make_tree (rca::Group&, rca::Network &, 
 				  std::shared_ptr<SteinerTree> & st);
+	
+	//this method update the priority_queue 
+	void update_best_edge (int node, double value, priority_queue_edge&, rca::Network&);
 	
 };
 
