060bd917e3382f468c5a11b02058c21d1d27e09d
Romerito Campos <romerito.campos@gmail.com>
2014-08-26 12:25:50 -0300

    methods set_modificator, remove_edges_by_mean and a variable added to bstmean.h

--- mpp_algorithms/myalgorithm/bstmean.h
+++ mpp_algorithms/myalgorithm/bstmean.h
@@ -2,6 +2,9 @@
 #define _BSTMEAN_
 
 #include <iostream>
+#include <memory>
+#include <queue>
+#include <functional>
 
 #include "network.h"
 #include "link.h"
@@ -20,12 +23,25 @@ public:
 	void make_tree (rca::Group&, rca::Network &, 
 				  std::shared_ptr<SteinerTree> & st);
 	
+	inline void set_mean_moficator (double value) {
+		m_mean_modificator = value;
+	}
+	
 private:	
 	//removendo arestas pela média
-	double remove_edges_by_mean (rca::Network &);
+	void remove_edges_by_mean (rca::Network &,
+							   rca::Group &, 
+								std::vector<rca::Link>&);
 	
 	//this method update the priority_queue 
 	void update_best_edge (int node, double value, priority_queue_edge&, rca::Network&);
+	
+	//variables
+private:
+	
+	//value used to increase the value of mean.
+	//the value of mean is used to separete edges to be removed
+	double m_mean_modificator;
 	
 };
 
