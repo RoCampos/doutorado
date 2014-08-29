2f6d07bfd1831fa50b7fd0f07ac965a0a96c8096
Romerito Campos <romerito.campos@gmail.com>
2014-08-29 14:23:41 -0300

    teste_properties used to test ACO

--- mpp_algorithms/myalgorithm/teste_properties.cpp
+++ mpp_algorithms/myalgorithm/teste_properties.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <memory>
 
+#include "aco.h"
+#include "aco.cpp"
+#include "acompp.h"
+
+using namespace rca;
+
 int main (int argv, char **argc) {
     
+	std::string str = argc[1];
+	
+	ACO<AcoMPP> algorithm;
+	algorithm.set_instance (str);
+	algorithm.run ();
+	
 	return 0;
 }
