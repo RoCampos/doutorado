0b830d29bc5203d1a39aa49f5c00d1a6b67be711
Romerito Campos <romerito.campos@gmail.com>
2014-08-29 13:54:29 -0300

    implementation of configurate function

--- mpp_algorithms/myalgorithm/acompp.cpp
+++ mpp_algorithms/myalgorithm/acompp.cpp
@@ -2,26 +2,32 @@
 
 using namespace rca;
 
-void AcoMPP::update_pheromone () const {
+void AcoMPP::update_pheromone () {
 
 	std::cout << "Updating Pheromene\n";
 	
 }
 
-void AcoMPP::solution_construction () const {
+void AcoMPP::solution_construction () {
 
 	std::cout << "solution_construction\n";
 	
 }
 
-void AcoMPP::initialization () const {
+void AcoMPP::initialization () {
 	
 	std::cout << "initialization\n";
 	
 }
 
-void AcoMPP::configurate (std::string m_instance) const{
+void AcoMPP::configurate (std::string m_instance) {
 
 	std::cout << "configuring data\n";
+	Reader r(m_instance);
+	
+	m_network = new rca::Network;
+	
+	r.configNetwork (m_network);
+	m_groups = r.readerGroup ();
 	
 }
