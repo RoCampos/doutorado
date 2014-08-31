bf84566cb9ea81eaf62ec2ea1c1b04d92bd9fe22
Romerito Campos <romerito.campos@gmail.com>
2014-08-31 15:49:37 -0300

    Implementations file added to ant.hh

---
+++ mpp_algorithms/myalgorithm/ant.cpp
@@ -0,0 +1,19 @@
+#include "ant.h"
+
+using namespace rca;
+
+void Ant::move () {
+
+#ifdef DEBUG
+	std::cout << "Doing move ()\n";
+#endif 
+	
+}
+
+void Ant::join (const Ant & move) {
+
+#ifdef DEBUG
+	std::cout << "Doing join ()\n";
+#endif
+	
+}
