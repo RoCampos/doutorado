dead2ee3fa6cc9e14809df372ce4438256a13b11
Romerito Campos <romerito.campos@gmail.com>
2014-08-31 16:26:28 -0300

    new methods added to ant.h

--- mpp_algorithms/myalgorithm/ant.h
+++ mpp_algorithms/myalgorithm/ant.h
@@ -1,11 +1,14 @@
-#ifndef _ANT_H_
-#define _ANT_H_
+
 
 #include <iostream>
 #include <vector>
 
+#include "link.h"
+
 namespace rca {
 
+#ifndef _ANT_H_
+#define _ANT_H_
 /**
  * This class represents a Ant component
  * of ACO(Ant Colony Optimizatin). 
@@ -25,17 +28,22 @@ public:
 	Ant (int id) : m_id (id){};
 	
 	//method that makes the ant moving forward in the graph
-	void move ();
+	void move (int to);
 	
 	//method to join an ant to another in the search
 	void join (const Ant & ant);
-
+	
+	int get_current_position () const;
+	
+	int get_id () const;
+	
+	
 private:
 	//id of the node. The path start by the id	
 	int m_id;
 	
 	//current position
-	int m_current_vertex;
+	int m_current_pos;
 	
 	//nodes searched by m_id
 	std::vector<int> m_nodes;
@@ -43,6 +51,10 @@ private:
 	//links in the solution
 	std::vector<rca::Link> links;
 	
+	
 };
 
+#endif
+
 }
+
