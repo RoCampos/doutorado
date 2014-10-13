c20f59727ec8b7f312f36ab337c6c704972558d6
Romerito Campos <romerito.campos@gmail.com>
2014-10-13 10:04:23 -0300

    files to KMB algorithm added. The Base policy design have been used

---
+++ stalgorithm/kmb.cpp
@@ -0,0 +1,10 @@
+#include "kmb.h"
+
+void KMB::heuristic (rca::Network & net, SteinerTree & st, rca::Group & g) {
+
+	printf ("%s\n", "Running KMB heuristic!");
+	
+	
+	
+	
+}
---
+++ stalgorithm/kmb.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "network.h"
+#include "steinertree.h"
+#include "algorithm.h"
+
+class KMB {
+
+protected:
+	
+	void heuristic (rca::Network & net, SteinerTree & st, rca::Group & g);
+	
+};
