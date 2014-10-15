dbe30d922b1a3cd492fdd3b66f5bde95c95d8459
Romerito Campos <romerito.campos@gmail.com>
2014-10-15 13:05:37 -0300

    script to run kmb heuristc and obtain preprocessed files

--- scripts/kmbpre.sh
+++ scripts/kmbpre.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#This file generate the files *.pre based on results from KMB algorithm
+
+# ./scritpt ../MPP-Instances/n30/ ../<saida>
+
+#instances de entrada
 input_dir=$1
+
+#diretórrio de saída
 output_dir=$2
+
 
 instances=`ls -vH ${input_dir}`
 
@@ -13,7 +21,7 @@ do
 	touch ${output_dir}${out}
 
 	#generating a preprocessing file based on a kmb algorithm
-	./build/stalgo_test $input_dir${i} $output_dir$out
+	./build/kmbopt $input_dir${i} $output_dir$out
 	cat ${output_dir}${out}
 
 	
