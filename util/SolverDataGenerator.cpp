7bdc8cd666c07426f56846eda6fb5c7d37c876df
Romerito Campos <romerito.campos@gmail.com>
2015-02-20 10:37:34 -0300

    SolverDataGenerator framework for MILP solvers added

---
+++ util/SolverDataGenerator.cpp
@@ -0,0 +1,138 @@
+#include "SolverDataGenerator.h"
+
+void MultiCommidityFormulation::generate (rca::Network * network, 
+										  rca::Group *g)
+{
+	
+	std::cout << "set V := ";
+	for (int i=0;i < network->getNumberNodes (); i++) {
+		std::cout << i + 1 << " ";
+	}
+	std::cout << ";\n";
+
+	std::cout << "\nset LINKS :=";
+	for (int i=0; i < network->getNumberNodes (); i++) {
+		for (int j=0; j < i; j++) {
+			
+			if (network->getCost (i,j) > 0.0) {
+				std::cout <<"\n\t(" <<  i+1 << "," << j+1 << ")";
+				std::cout <<"\n\t(" <<  j+1 << "," << i+1 << ")";
+			}
+			
+		}	
+	}
+	std::cout << " ;\n";
+	
+	std::cout << "\nset T := ";
+	for (int i=0; i < g->getSize (); i++) {
+		std::cout << " " << g->getMember (i) + 1;
+	}
+	std::cout << " ;\n";
+	
+	std::cout << "param r := " << g->getSource ()+1 << ";\n";
+	
+	std::cout << "param : cost delay:=";
+	for (int i=0; i < network->getNumberNodes (); i++) {
+		for (int j=0; j < i; j++) {
+			
+			if (network->getCost (i,j) > 0.0) {
+				std::cout <<"\n\t" <<  i+1 << "\t" << j+1;
+				std::cout <<"\t" << network->getCost (i,j);
+				std::cout <<"\t" << network->getBand (i,j);
+				
+				std::cout <<"\n\t" <<  j+1 << "\t" << i+1;
+				std::cout <<"\t" << network->getCost (i,j);
+				std::cout <<"\t" << network->getBand (i,j);
+			}			
+
+		}	
+	}
+	std::cout << ";\n";
+}
+
+void MultipleMulticastCommodityFormulation::generate (rca::Network * network, 
+													  std::vector<std::shared_ptr<Group>> & groups)
+{
+	//gerando o conjunto de vértices
+	std::cout << "set V := ";
+	for (int i=0;i < network->getNumberNodes (); i++) {
+		std::cout << i + 1 << " ";
+	}
+	std::cout << ";\n";
+	
+	//gerando o conjunto de arestas
+	std::cout << "\nset LINKS :=";
+	for (int i=0; i < network->getNumberNodes (); i++) {
+		for (int j=0; j < i; j++) {
+			
+			if (network->getCost (i,j) > 0.0) {
+				std::cout <<"\n\t(" <<  i+1 << "," << j+1 << ")";
+				std::cout <<"\n\t(" <<  j+1 << "," << i+1 << ")";
+			}
+			
+		}	
+	}
+	std::cout << " ;\n";
+	
+	//gerando param de groups
+	std::cout << "set GROUPS := ";
+	for (unsigned int i=0; i < groups.size (); i++) {
+		std::cout << " " << i + 1;
+	}
+	std::cout << ";\n";
+	
+	//gerando param de raízes
+	std::cout << "param Mroot := \n";
+	for (unsigned int i=0; i < groups.size (); i++) {
+		std::cout << "\t" << i+1 << " " << groups[i]->getSource () + 1<<"\n";
+	}
+	std::cout << ";\n";
+	
+	std::cout << "set MEMBER := \n";
+	for (unsigned int i=0; i < groups.size (); i++) {
+		
+		std::vector<int> members = groups[i]->getMembers ();
+		for (unsigned int j = 0; j < members.size (); j++) {
+			
+			std::cout << "\t" << groups[i]->getSource ()+1 << " " << (members[j] + 1) << " "; 
+			
+		}
+		
+	}
+	
+	//gerando parametros de cost e delay
+	std::cout << "param : cost cap:=";
+	for (int i=0; i < network->getNumberNodes (); i++) {
+		for (int j=0; j < i; j++) {
+			
+			if (network->getCost (i,j) > 0.0) {
+				std::cout <<"\n\t" <<  i+1 << "\t" << j+1;
+				std::cout <<"\t" << network->getCost (i,j);
+				std::cout <<"\t" << network->getBand (i,j);
+				
+				std::cout <<"\n\t" <<  j+1 << "\t" << i+1;
+				std::cout <<"\t" << network->getCost (i,j);
+				std::cout <<"\t" << network->getBand (i,j);
+			}			
+
+		}	
+	}
+	
+	
+	
+}
+
+int main (int argc, char**argv) {
+
+	rca::SteinerReader r (argv[1]);
+	Network * net = new Network;
+	
+	r.configNetwork ( net );
+	
+	Group g = r.terminalsAsGroup ();
+	
+	DataGenerator<MultiCommidityFormulation> dg;
+	dg.run (net, &g);	
+	
+	return 0;
+}
---
+++ util/SolverDataGenerator.h
@@ -0,0 +1,47 @@
+#ifndef _SOLVER_DATA_GENERATOR_H_
+#define _SOLVER_DATA_GENERATOR_H_
+
+#include <iostream>
+#include <ostream>
+
+#include "group.h"
+#include "network.h"
+#include "reader.h"
+#include "steinerReader.h"
+
+template <typename Model>
+class DataGenerator : private Model
+{
+	using Model::generate;
+	
+public:
+	
+	void run (rca::Network * network, rca::Group *g) {
+		
+		generate (network, g);
+		
+	}
+	
+	
+};
+
+
+class MultiCommidityFormulation 
+{
+	
+public:
+	
+	void generate (rca::Network * network, rca::Group *g) ;
+	
+};
+
+class MultipleMulticastCommodityFormulation
+{
+
+public:
+	void generate (rca::Network * network, 
+					std::vector<std::shared_ptr<Group>>&);
+	
+};
+
+#endif
