eb8b4eb4c3bd2850e61c726f2d2db0b7a70fc56f
Romerito Campos <romerito.campos@gmail.com>
2015-02-24 22:43:22 -0300

    steiner_tree test added. The connectivity test must be implemented

--- util/SolverSolutionTest.cpp
+++ util/SolverSolutionTest.cpp
@@ -10,7 +10,8 @@ bool MMMSTPGurobiResult::do_test (std::string instance, std::string result, int 
 
 	
 	int obj = objective_test (net, groups, result);
-	
+	bool tree = steiner_tree_test (net, groups, result);
+	std::cout << obj << " " << (tree ? "true": "false") << "\n";
 	
 	delete net;
 	
@@ -59,10 +60,8 @@ int MMMSTPGurobiResult::objective_test (rca::Network * net,
 		str.erase (str.end() - 3, str.end());
 		w = stoi (str);
 		
-		//std::cout << v-1 << "|" << w-1 << "|"<<g-1<<"--";
-		
+		//updating the consuption
 		matrix[ v-1 ][ w-1 ] -= groups[g-1]->getTrequest ();
-		std::cout << matrix[ v-1 ][ w-1 ] << std::endl;
 		
 	}
 	
@@ -82,6 +81,86 @@ int MMMSTPGurobiResult::objective_test (rca::Network * net,
 	return min;
 }
 
+bool MMMSTPGurobiResult::steiner_tree_test (rca::Network * net, 
+										 list_groups groups, 
+										 std::string result)
+{
+
+	std::vector<int> nodes = std::vector<int> (net->getNumberNodes (), 0);
+	
+	for (int i=0; i < groups.size (); i++) {
+		
+		//std::cout << *groups[i].get() << std::endl;
+		
+		std::cout << "\tChecking steiner tree( " << i << ")\n";
+		
+		//verify if leafs are terminals
+		std::ifstream file (result);
+		std::string line;
+		while ( getline (file, line) )
+		{	
+			int v = -1;
+			int w = -1;
+			
+			std::stringstream ss (line);
+			
+			//getting v
+			ss >> v;
+			
+			std::string str;
+			ss >> str;
+			ss >> str;
+					
+			//getting the groups
+			char& r = str[ str.size ()-2 ];
+			char * s = &r;
+			int g = atoi(s);
+			
+			//geting w
+			str.erase (str.end() - 3, str.end());
+			w = stoi (str);
+					
+			if ((g - 1) == i){
+				nodes[w-1]++;
+				nodes[v-1]++;
+			} else {
+				break;
+			}
+			
+		}
+		
+		//checking terminals
+		int count = 0;
+		for (unsigned int j=0; j < nodes.size (); j++) {
+			
+			if (nodes[j] >= 0 && 
+				(groups[i]->isMember( j ) || j == groups[i]->getSource()) )
+			{
+				count++;
+			}			
+		}
+		//checking terminals
+		assert (count == (groups[i]->size () + 1) );
+		std::cout << "\tterminals number: "<< count << "\n";
+		
+		bool flag = true;
+		for (unsigned int j=0; j < nodes.size (); j++) {
+		
+			if (nodes[j] == 1 && !(groups[i]->isMember ( j )) )
+			{
+				flag = false;
+				break;
+			}			
+		}
+		assert ( flag );
+		std::cout << "\tthere is no non-terminals as leaf\n";
+	
+	}
+	//count the number of terminals 
+	//verify connectivty
+	
+	return true;
+}
 
 int main (int argv, char**argc)
 {
