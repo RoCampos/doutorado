e0b4c298da2a2aaaa58a2f060907dbdf38f0e931
Romerito Campos <romerito.campos@gmail.com>
2015-03-03 19:26:55 -0300

    init_population implemented

--- mmmstp/genetic_algorithm.cpp
+++ mmmstp/genetic_algorithm.cpp
@@ -8,6 +8,10 @@ void GeneticAlgorithm::run_metaheuristic (std::string instance)
 	m_network = new Network;
 	Reader r (m_instance);
 	r.configNetwork ( m_network );
+	std::vector<shared_ptr<rca::Group>> g = r.readerGroup ();
+	for (unsigned int i =0; i < g.size (); i++) {
+		m_groups.push_back (*g[i].get ());
+	}
 	
 	/*configuring parameters - default*/
 	init_parameters ();
@@ -26,6 +30,8 @@ void GeneticAlgorithm::init_population ()
 #ifdef DEBUG
 	std::cout << "Creating Population: size = " << m_pop << std::endl;	
 #endif
+	
+	m_population = std::vector<PathRepresentation> (m_pop);
 	for (int i=0; i < m_pop; i++) {
 		m_population[i].init_rand_solution (m_network, m_groups);		
 	}
@@ -35,16 +41,28 @@ void GeneticAlgorithm::init_population ()
 void PathRepresentation::init_rand_solution (rca::Network * net, 
 									  std::vector<rca::Group>& groups)
 {
-	int GROUPS = groups.size ();
+#ifdef DEBUG
+	std::cout << "genotype size = ";
+	int count = 0;
+#endif
 	
+	int GROUPS = groups.size ();
 	for (int i=0; i < GROUPS; i++) {
 		int source = groups[i].getSource ();
-		for (int d=0; d < groups[i].getSize (); i++) {
+#ifdef DEBUG
+	count += groups[i].getSize ();
+#endif
+		for (int d=0; d < groups[i].getSize (); d++) {
+			
 			int w = groups[i].getMember (d);
 			rca::Path path = shortest_path (source, w, net);
-			std::cout << path << std::endl;
+			m_genotype.push_back (path);
 		}
 	}
+	
+#ifdef DEBUG
+	std::cout << (count) << std::endl;
+#endif
 	
 }
 
