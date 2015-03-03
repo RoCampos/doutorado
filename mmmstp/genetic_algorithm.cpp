bd789091f4c0225eeeaf6e006050ceccf05d737e
Romerito Campos <romerito.campos@gmail.com>
2015-03-03 19:14:12 -0300

    init_rand_solution started

--- mmmstp/genetic_algorithm.cpp
+++ mmmstp/genetic_algorithm.cpp
@@ -1,9 +1,50 @@
+#include "genetic_algorithm.h"
 #include "metaheuristic.h"
 
 void GeneticAlgorithm::run_metaheuristic (std::string instance)
 {
+	/*configuring the problem*/
+	m_instance = instance;
+	m_network = new Network;
+	Reader r (m_instance);
+	r.configNetwork ( m_network );
 	
-	std::cout << instance << std::endl;
+	/*configuring parameters - default*/
+	init_parameters ();
+	
+	/*init population*/
+	init_population ();
+	
+	
+	
+	//deallocatin of resources;
+	delete m_network;
+}
+
+void GeneticAlgorithm::init_population ()
+{
+#ifdef DEBUG
+	std::cout << "Creating Population: size = " << m_pop << std::endl;	
+#endif
+	for (int i=0; i < m_pop; i++) {
+		m_population[i].init_rand_solution (m_network, m_groups);		
+	}
+	
+}
+
+void PathRepresentation::init_rand_solution (rca::Network * net, 
+									  std::vector<rca::Group>& groups)
+{
+	int GROUPS = groups.size ();
+	
+	for (int i=0; i < GROUPS; i++) {
+		int source = groups[i].getSource ();
+		for (int d=0; d < groups[i].getSize (); i++) {
+			int w = groups[i].getMember (d);
+			rca::Path path = shortest_path (source, w, net);
+			std::cout << path << std::endl;
+		}
+	}
 	
 }
 
@@ -12,7 +53,6 @@ int main (int argc, char**argv)
 
 	std::string instance = argv[1];
 	MetaHeuristic<GeneticAlgorithm> algorithm;
-	
 	
 	
 	algorithm.run (instance);
