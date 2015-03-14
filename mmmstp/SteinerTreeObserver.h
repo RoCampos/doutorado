f0ad9fae79161b6ee560caa53190f1ee2aa6d48b
Romerito Campos <romerito.campos@gmail.com>
2015-03-14 13:55:00 -0300

    new approach for compute solution congestion

--- mmmstp/SteinerTreeObserver.h
+++ mmmstp/SteinerTreeObserver.h
@@ -3,8 +3,11 @@
 
 #include "steinertree.h"
 
+
+class SteinerTreeObserver;
+
 class CongestionHandle {
-	
+	friend SteinerTreeObserver;
 public:
 	
 	CongestionHandle (){}
@@ -12,33 +15,57 @@ public:
 	CongestionHandle (rca::Network * net, std::vector<rca::Group> & g){
 		m_network = net;
 		m_groups = g;
-		
+		/*
 		int NODES = m_network->getNumberNodes ();
 		m_congestion = std::vector<std::vector<int>> (NODES);
 		for (int i=0;i < NODES; i++) {
-			m_congestion[i] = std::vector<int> (NODES);
+			m_congestion[i] = std::vector<int> (NODES,0);
 		}
 		
 		for (int i=0; i < NODES; i++) {
 			for (int j=0; j < NODES;j++) {
-				m_congestion[i][j] = m_network->getBand (i,j);
+				if (m_network->getCost (i,j) > 0)
+					m_congestion[i][j] = m_network->getBand (i,j);
 			}
-		}
+		}*/
 	}
 	
 	void add_edge (int i,int j,int group) {
 	
-		m_congestion[i][j] -= m_groups[group].getTrequest ();
-		m_congestion[j][i] = m_congestion[i][j];
+		int trequest = m_groups[group].getTrequest ();
+		rca::Link link (i,j,0);
 		
+		std::vector<rca::Link>::iterator it;
+		it = std::find (m_used_links.begin (), m_used_links.end(),link);
+		
+		if (it != m_used_links.end()) {
+			
+			it->setValue ( it->getValue () - trequest);
+			
+		} else {
+			
+			int band = m_network->getBand (link.getX(), link.getY());
+			link.setValue (band - trequest);
+			m_used_links.push_back (link);
+		}
 	}
+
 	void remove_edge (int i, int j, int group) {
-		m_congestion[i][j] += m_groups[group].getTrequest ();
-		m_congestion[j][i] = m_congestion[i][j];
+		
+		int trequest = m_groups[group].getTrequest ();
+		rca::Link link (i,j,0);
+		
+		std::vector<rca::Link>::iterator it;
+		it = std::find (m_used_links.begin (), m_used_links.end(),link);
+		
+		if (it != m_used_links.end()) {
+			it->setValue ( it->getValue () + trequest);
+		}
+		
 	}
 	
 	int congestion () {
-		
+		/*
 		int max = 9999;
 		for (int i=0; i < (int)m_congestion.size (); i++) {
 			for (int j=0; j < (int) m_congestion[i].size(); j++ ) {
@@ -51,15 +78,18 @@ public:
 				}
 				
 			}
-		}
-		return max;
+		}*/
+		
+		return ( std::min_element (m_used_links.begin(), 
+								 m_used_links.end())  )->getValue();
 	}
 	
 private:
-	std::vector<std::vector<int>> m_congestion;
+	//std::vector<std::vector<int>> m_congestion;
 	rca::Network * m_network;
 	std::vector<rca::Group> m_groups;
 	
+	std::vector<rca::Link> m_used_links;
 	
 };
 
@@ -68,10 +98,12 @@ class SteinerTreeObserver {
 public:
 	
 	//receive a steiner tree and Observer
-	SteinerTreeObserver (SteinerTree* st, CongestionHandle& ch) {
+	SteinerTreeObserver (SteinerTree* st, CongestionHandle * ch) {
 		m_st = st;
 		m_ch = ch;
 		m_cost = 0;
+		
+		num_links = 0;
 	}
 	
 	void setTree (SteinerTree * st) {
@@ -79,10 +111,12 @@ public:
 	}
 	
 	void addEdge (int i,int j, int value, int group) {
-		m_st->addEdge (i,j,value);
-		m_cost += value;
 		
-		m_ch.add_edge (i,j,group);
+		m_st->addEdge (i,j,value);
+		num_links++;
+		m_cost += value;
+		m_ch->add_edge (i,j,group);
+		
 	}
 	
 	int getCost () {
@@ -112,7 +146,9 @@ public:
 			
 			if (edge) {
 				m_cost -= edge->cost;
-				m_ch.remove_edge (edge->i, edge->j, group);
+				m_ch->remove_edge (edge->i, edge->j, group);
+				
+				num_links--;
 			}
 			
 			//remover aresta
@@ -151,10 +187,15 @@ public:
 		
 	}
 	
+public:
+	int num_links;
+	
 private:
 	SteinerTree * m_st;
-	CongestionHandle m_ch;
+	CongestionHandle * m_ch;
 	int m_cost;
+	
+	
 };
 
 #endif
