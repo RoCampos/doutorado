ce7649b4a8e80b4ee92b4246daf7b40d218325c8
Romerito Campos <romerito.campos@gmail.com>
2015-03-18 13:18:32 -0300

    SteinerTreeObserver has a capacited shortest path

--- mmmstp/SteinerTreeObserver.h
+++ mmmstp/SteinerTreeObserver.h
@@ -1,12 +1,24 @@
 #ifndef _STTREE_OBSERVER_H_
 #define _STTREE_OBSERVER_H_
 
+#include <vector>
+#include <climits>
+#include <algorithm>
+
 #include "steinertree.h"
 #include "disjointset2.h"
-
+#include "heapfibonnaci.h"
 
 class SteinerTreeObserver;
 
+/**
+ * Esta classe mantém a lista de arestas que estão sendo usandas
+ * em uma solução. Com ela é possível controlar a capacidade residual
+ * de cada aresta.
+ * 
+ * @author Romerito Campos
+ * @date 03/18/2015
+ */
 class CongestionHandle {
 	friend SteinerTreeObserver;
 public:
@@ -31,6 +43,11 @@ public:
 		return *this;
 	}
 	
+	/**
+	 * Método registrar que uma aresta está sendo utilizada pelo grupo group.
+	 * 
+	 * Este método leva O(E).
+	 */
 	void add_edge (int i,int j,int group) {
 	
 		int trequest = m_groups[group].getTrequest ();
@@ -51,6 +68,12 @@ public:
 		}
 	}
 
+	/**
+	 * Este método é utilizado para informar que uma aresta não está sendo
+	 * utilizada pelo grupo group.
+	 * 
+	 * O(E).
+	 */
 	void remove_edge (int i, int j, int group) {
 		
 		int trequest = m_groups[group].getTrequest ();
@@ -65,6 +88,19 @@ public:
 		
 	}
 	
+	int getResidualCapacity (rca::Link & l) {
+	
+		auto it = std::find(m_used_links.begin (), m_used_links.end(), l);
+		
+		int n = INT_MIN;
+		return (it != m_used_links.end() ? it->getValue() : n);
+	}
+	
+	/**
+	 * Retorna a capacidade residual mínima
+	 * 
+	 * O( sort ) + 1; 
+	 */
 	int congestion () {
 		
 		std::sort (m_used_links.begin(), m_used_links.end());
@@ -211,5 +247,96 @@ private:
 	
 	
 };
+
+rca::Path capacited_shortest_path (int v, int w, 
+								   rca::Network *network,
+								   CongestionHandle *cg)
+{
+
+	typedef FibonacciHeapNode<int,double> Element; //todo VErificar se é double ou int 1
+
+	int NODES = network->getNumberNodes();
+
+	vector< int > prev = vector< int >( NODES );
+	vector< double >distance = vector< double >( NODES ); //acho que tinha um erro aqui
+	vector< bool > closed = vector< bool >( NODES );
+
+	FibonacciHeap < int, double > queue; // todo VErificar se é double ou int 2
+	Element* elem = NULL;
+	int register i;
+	for (i=0; i < NODES; i++) {
+		prev[i] = -1;
+		distance[i] = 9999999;
+		if (i == v)
+			elem = queue.insert (i,9999999);
+		closed[i] = false;
+	}
+
+	distance[v] = 0;
+	queue.decreaseKey (elem,0);
+
+	double cost = 0.0;
+	bool removed = false;
+	while (!queue.empty()) {
+
+		int e = queue.minimum ()->data ();
+		queue.removeMinimum ();
+
+		closed[e] = true;
+
+		//i is defined as register variable
+		for (i=0; i < NODES; ++i) {
+
+			
+			cost = network->getCost (e, i);
+			
+			if (cost > 0.0) {
+				
+				removed = network->isRemoved(rca::Link(e, i, cost));
+			
+				if (!closed[i] && cost > 0
+					&& (distance[i] > (distance[e] + cost))
+					&& !network->isVertexRemoved(i) 
+					&& !removed) 
+				
+				{
+					distance[i] = distance[e] + cost;
+					prev[i] = e;
+					queue.insert (i, distance[i]);
+				}
+			}
+			
+		}
+		
+	}
+
+	delete elem;
+
+	rca::Path path;
+	int pos = 0;
+	double pathcost = 0.0;
+	int s = w;
+	while (s != v) {
+		path.push (s);
+		s = prev[s];
+		
+		if (s == -1 || s >= NODES)
+		{
+			rca::Path path2;
+			return path2; //se não há caminho
+		}
+		
+		
+		pathcost += network->getCost (path[pos],s);
+		
+		pos++;
+
+	}
+	path.push (s);
+	path.setCost (pathcost); //definindo o custo
+	
+	return path;
+	
+}
 
 #endif
