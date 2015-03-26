78ba054c04e096135cf00e5da17aa7c7ac343a3f
Romerito Campos <romerito.campos@gmail.com>
2015-03-26 16:46:00 -0300

    print method in chen's algorithm has been changed

--- mpp_algorithms/Chen2000/chenalgorithm.h
+++ mpp_algorithms/Chen2000/chenalgorithm.h
@@ -41,7 +41,8 @@ typedef struct sttree {
 	
 	void print () {
 		for (auto it = edges.begin(); it != edges.end(); it++) {
-			cout << *it << endl;
+			cout << it->getX ()+1 << " - ";
+			cout << it->getY ()+1 << ":" << this->id + 1 << ";"<< endl;
 		}
 	}
 	
