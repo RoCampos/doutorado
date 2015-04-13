6970d7a8d47de77636a823ec951c825863a0868d
Romerito Campos <romerito.campos@gmail.com>
2015-04-13 11:27:51 -0300

    SteinerTreeObserver added to sttalgo lib

---
+++ include/steiner_tree_observer.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "edgecontainer.h"
+#include "sttree.h"
+#include "disjointset2.h"
+#include "sttree_visitor.h"
+#include "link.h"
+
+namespace rca {
+
+#ifndef _STEINER_TREE_OBSERVER_
+#define _STEINER_TREE_OBSERVER_
+	
+template<typename ContainerType>
+class SteinerTreeObserver {
+
+public:
+	SteinerTreeObserver();
+	SteinerTreeObserver(ContainerType & ec, STTree & st, int);
+	
+	void set_steiner_tree (STTree &st, int);
+	STTree & get_steiner_tree ();
+	
+	void set_container (ContainerType & ec);
+	ContainerType & get_container ();
+	
+	bool add_edge (int, int, int, int);
+	
+	std::vector<rca::Link> getTreeAsLinks () const;
+	
+	void prune (int, int);
+
+private:
+	ContainerType * m_ec;
+	STTree m_st;	
+	DisjointSet2 * dset;
+	
+};
+
+#endif
+
+};
+
---
+++ stalgorithm/steiner_tree_observer.cpp
@@ -0,0 +1,107 @@
+#include "steiner_tree_observer.h"
+
+using namespace rca;
+
+template<typename ContainerType>
+SteinerTreeObserver<ContainerType>::SteinerTreeObserver(){
+	dset = NULL;
+}
+
+template<typename ContainerType>
+SteinerTreeObserver<ContainerType>::SteinerTreeObserver(ContainerType & ec,
+														STTree & st,
+														int nodes)
+{
+	m_ec = &ec;
+	this->set_steiner_tree (st, nodes);
+	
+}
+
+template<typename ContainerType>
+void SteinerTreeObserver<ContainerType>::set_steiner_tree (STTree & st, int nodes)
+{
+	m_st = st;
+	
+	if (dset != NULL)
+		delete dset;
+	
+	dset = new DisjointSet2 (nodes);
+}
+
+template<typename ContainerType>
+STTree & SteinerTreeObserver<ContainerType>::get_steiner_tree ()
+{
+	return m_st;
+}
+
+template<typename ContainerType>
+void SteinerTreeObserver<ContainerType>::set_container (ContainerType & ec) 
+{
+	m_ec = &ec;
+}
+
+template<typename ContainerType>
+ContainerType & SteinerTreeObserver<ContainerType>::get_container () 
+{
+	return *m_ec;
+}
+
+template<typename ContainerType>
+bool SteinerTreeObserver<ContainerType>::add_edge (int x, 
+												   int y, 
+												   int cost, 
+												   int band_usage)
+{
+	
+	if ( dset->find(x) != dset->find(y) ) {
+	
+		dset->simpleUnion (x, y);
+		rca::Link l (x,y, cost);
+		
+		//modificar add_edge in STTree
+		//para verificar se a arestas já foi adicionada
+		//verificar com debug se o dset já evita isto
+		m_st.add_edge ( l.getX(), l.getY(), cost );
+		
+
+		//TODO UPDATE USAGE, IT IS USING THE COST		
+		if (m_ec->is_used (l)) {
+			int value = m_ec->value (l);
+			l.setValue (value - 1);
+		} else {
+			l.setValue (band_usage - 1);
+			m_ec->push ( l );
+		}
+		
+		return true;
+	}
+	
+	return false;	
+}
+
+template<typename ContainerType>
+std::vector<rca::Link> SteinerTreeObserver<ContainerType>::getTreeAsLinks () const
+{
+	std::vector<rca::Link> links;
+	
+	edge_t * perc = m_st.get_edge ();
+	while (perc != NULL) {
+		
+		rca::Link l(perc->x, perc->y, perc->value);		
+		if (perc->in)			
+			links.push_back ( l );
+		
+		perc = perc->next;
+		
+	}	
+	
+	return links;
+}
+
+template<typename ContainerType>
+void SteinerTreeObserver<ContainerType>::prune (int rest, int band)
+{
+	prunning<ContainerType>(m_st, *m_ec, rest, band); 
+}
+
+template class SteinerTreeObserver<EdgeContainer<Comparator, HCell>>;
