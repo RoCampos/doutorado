d937c2491d468853409233460013364d6ffe3585
Romerito Campos <romerito.campos@gmail.com>
2015-06-09 09:57:55 -0300

    limite has been documented

--- mmmstp/limite.h
+++ mmmstp/limite.h
@@ -12,6 +12,16 @@
 #include "edgecontainer.h"
 #include "steiner_tree_observer.h"
 
+/**
+ * Computes a steiner tree for each group. The algorithm build
+ * a spanning minimal tree for each group and make prune on non-terminal
+ * leaf nodes. The algorithm do not consider the residual capacity contraint.
+ * 
+ * The value could be a good lower bound for big instances.
+ * 
+ * @param std::vector<rca::Group>
+ * @param rca::Network
+ */
 double kruskal_limits (std::vector<rca::Group> &, rca::Network &);
 
 #endif /*_LIMITS_H_*/
