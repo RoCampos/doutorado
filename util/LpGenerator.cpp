f6a3e3753556fe63f626696c283a0333cd9533aa
Romerito Campos <romerito.campos@gmail.com>
2015-08-11 15:32:21 -0300

    LpGenerator is used based on option: can generate any of my models

--- util/LpGenerator.cpp
+++ util/LpGenerator.cpp
@@ -5,6 +5,9 @@
 #include "network.h"
 #include "reader.h"
 #include "steinerReader.h"
+
+void help ();
+
 /**
  * Binário que gera os lps para cada instância do MPP
  * 
@@ -13,7 +16,14 @@
  */
 int main (int argv, char**argc)
 {
-	std::string file = argc[1];
+	int option = atoi(argc[1]);
+	
+	if ( (strcmp(argc[1], "-h") == 0) ) {
+		help ();
+		exit (1);
+	}
+	
+	std::string file = argc[2];
 	
 	Network * net = new Network;
 	std::vector<shared_ptr<rca::Group>> g;
@@ -28,79 +38,61 @@ int main (int argv, char**argc)
 #ifdef MODEL_UNIT
 	r.configure_unit_values (net,g);
 #endif
-	/*
-	Network * net = new Network(6,10);
-	net->setCost (0,1,1);
-	net->setCost (0,3,1);
-	net->setCost (0,4,1);
-	net->setCost (0,5,1);
-	net->setCost (1,3,1);
-	net->setCost (1,4,1);
-	net->setCost (1,2,1);
-	net->setCost (2,3,1);
-	net->setCost (2,4,1);
-	net->setCost (2,5,1);
-	net->setCost (5,4,1);
 	
-	net->setCost (1,0,1);
-	net->setCost (3,0,1);
-	net->setCost (4,0,1);
-	net->setCost (5,0,1);
-	net->setCost (3,1,1);
-	net->setCost (4,1,1);
-	net->setCost (2,1,1);
-	net->setCost (3,2,1);
-	net->setCost (4,2,1);
-	net->setCost (5,2,1);
-	net->setCost (4,5,1);
+	switch (option) {
 	
-	net->setBand (0,1,2);
-	net->setBand (0,3,2);
-	net->setBand (0,4,2);
-	net->setBand (0,5,2);
-	net->setBand (1,3,2);
-	net->setBand (1,4,2);
-	net->setBand (1,2,2);
-	net->setBand (2,3,2);
-	net->setBand (2,4,2);
-	net->setBand (2,5,2);
-	net->setBand (5,4,1);
-	
-	net->setBand (1,0,2);
-	net->setBand (3,0,2);
-	net->setBand (4,0,2);
-	net->setBand (5,0,2);
-	net->setBand (3,1,2);
-	net->setBand (4,1,2);
-	net->setBand (2,1,2);
-	net->setBand (3,2,2);
-	net->setBand (4,2,2);
-	net->setBand (5,2,2);
-	net->setBand (4,5,1);
-	
-	Group g1(0,0,1);
-	Group g2(1,0,1);
-	Group g3(2,2,1);
-	
-	g1.addMember (1);
-	g1.addMember (3);
-	
-	g2.addMember (2);
-	g2.addMember (4);
-	
-	
-	std::vector<shared_ptr<rca::Group>> g;
-	g.push_back (make_shared<rca::Group>(g1) );
-	g.push_back (make_shared<rca::Group>(g2) );
-	//g.push_back (make_shared<rca::Group>(g3) );
-	*/
-	
-	//std::vector<shared_ptr<rca::Group>> g = r.readerGroup ();
-	
-// 	MMSTPCostLP dg;
-	MMSTPBudgetLP dg;		
-	int budget = atoi (argc[2]); //limite de orçamento
-	dg.generate2 (net, g, budget);
+		case 1: {
+			
+			MMSTPBudgetLP dg;		
+			int budget = atoi (argc[3]); //limite de orçamento
+			dg.generate2 (net, g, budget);
+			
+		}
+			
+		break;
+		
+		case 2: {
+			MMSTPCostLP dg;
+			int budget = atoi (argc[3]); //capacidade residual como limite
+			dg.generate2 (net, g, budget);
+			
+		}
+		break;
+		
+		case 3: {
+			
+			LeeAndChooModel dg;		
+			LeeAndChooModel::set_alpha (atoi (argc[3]));
+			LeeAndChooModel::set_opt (argc[4]);
+			
+			dg.generate (net, g);
+			
+		}
+	}
+
 
 	return 0;
+}
+
+void help ()
+{
+
+	std::string msg;
+	
+	msg = "\n\nThis app is used to create lp files to be used on Gurobi Solver\n";
+	msg += "There are some models implemented on it and they receive different\n";
+	msg += "of parameters.So if you have doubts type LpGenerator-h\n";
+	msg += "\n";
+	
+	msg += "Option:\n";
+	
+	msg += "\t1\n";
+	
+	msg += "\t2\n";
+	
+	msg += "\t3 - Lee and Cho Moodel: this model maximize residual capacity subject\n";
+	msg += "\t\tto alpha*OPT_k on each multicast tree where\n";
+	msg += "\t\tParameters: <option><brite><alpha><OPTFILE>\n";
+	
+	std::cout << msg << std::endl;
 }
