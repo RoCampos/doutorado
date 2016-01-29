e83e93b1448ceaf542666c3e257274fe65929f42
Romerito Campos <romerito.campos@gmail.com>
2016-01-29 12:26:11 -0300

    steinermodel.cpp for build binary steinermodel was created

---
+++ multi/steinermodel.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <boost/regex.hpp>
+#include "gurobi_c++.h"
+
+#include "models.h"
+#include "network.h"
+#include "group.h"
+#include "reader.h"
+
+int main(int argc, char const *argv[])
+{
+	
+	std::string file = argv[1];
+
+	rca::Network net;
+	std::vector<rca::Group> multicast_group;
+	std::vector<shared_ptr<rca::Group>> temp;	
+	
+	MultipleMulticastReader r (file);		
+	r.configure_real_values (&net,temp);
+	for (auto g : temp) {
+		multicast_group.push_back (*g);
+	}
+
+	GRBEnv env = GRBEnv ();
+	GRBModel model = GRBModel (env);
+
+
+	SteinerTreeModel st (model, net, multicast_group[0], 
+		SteinerMode::OPTIMIZE_BY_COST);
+
+
+	model.write ("teste.lp");
+
+
+	return 0;
+}
