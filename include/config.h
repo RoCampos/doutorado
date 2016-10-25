930edf869aa3a2d20455d7fd7c6591e0d113b101
Romerito Campos <romerito.campos@gmail.com>
2016-10-24 21:31:20 -0300

    execution message added

--- include/config.h
+++ include/config.h
@@ -6,19 +6,22 @@
 #include <cstdlib>
 
 #define VERSION_MAJOR 2
-#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
-void message (int argc, char const *argv[]) 
+bool message (int argc, char const *argv[], std::string command) 
 {
 	if (argc < 2) {
 		fprintf(stdout,"%s Version %d.%d.%d\n",
 			argv[0],
 			VERSION_MAJOR,
-			VERSION_MAJOR,
+			VERSION_MINOR,
 			VERSION_PATCH);
-		fprintf(stdout,"Usage: %s number\n",argv[0]);
+		fprintf(stdout,"Usage: %s %s\n",argv[0], command.c_str ());
+		return true;
 	}
+
+	return false;
 }
 
 #endif // CONFIG_H
